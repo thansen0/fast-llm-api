44e35dd087799f1835510ecfdf563dd78398bfbe
_ <thomash1996@live.com>
2024-10-30 12:37:27 -0700

    added file logging with zap

--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.com/thansen0/localllmapiserver
 go 1.22.5
 
 require (
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
 	google.golang.org/grpc v1.67.1 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 )
--- main.go
+++ main.go
@@ -6,27 +6,50 @@ import (
     "log"
     "net"
 
+    "go.uber.org/zap"
+    "gopkg.in/natefinch/lumberjack.v2"
+    "go.uber.org/zap/zapcore"
+
     "google.golang.org/grpc"
     pb "github.com/thansen0/localllmapiserver/protos"
 )
+
+var logger *zap.Logger
+
+// server is used to implement AskLLMQuestionServer
+type server struct {
+    pb.UnimplementedAskLLMQuestionServer
+}
+
+func initLogger() *zap.Logger {
+    writeSyncer := zapcore.AddSync(&lumberjack.Logger{
+        Filename:   "./logs/apirequests.log",
+        MaxSize:    10, // Megabytes before rotating
+        MaxBackups: 3,  // Number of old files to retain
+        MaxAge:     28, // Days to retain old log files
+        Compress:   true, // Compress rotated files
+    })
+
+    encoderConfig := zap.NewProductionEncoderConfig()
+    encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder // Human-readable timestamp
+    encoder := zapcore.NewJSONEncoder(encoderConfig)
+
+    core := zapcore.NewCore(encoder, writeSyncer, zapcore.InfoLevel)
+    return zap.New(core)
+}
+
+func verify_user(api_key string) bool {
+    return true
+}
+
+func savePromptAnswer(prompt string, api_key string, answer string) {
+    logger.Info(fmt.Sprintf("%s, %s, %s", api_key, prompt, answer))
+}
 
 func generate_llm_response(prompt string) string {
     fmt.Printf("Received prompt: %s\n", prompt)
 
     return "fake llama response"
-}
-
-func is_user(api_key string) bool {
-    return true
-}
-
-func savePromptAnswer(prompt string, api_key string, answer string) {
-    fmt.Println("Saving to log or DB")
-}
-
-// server is used to implement AskLLMQuestionServer
-type server struct {
-    pb.UnimplementedAskLLMQuestionServer
 }
 
 // PromptLLM implements AskLLMQuestion.PromptLLM
@@ -38,18 +61,22 @@ func (s *server) PromptLLM(ctx context.Context, request *pb.LLMInit) (*pb.LLMInf
         answerChannel <- generate_llm_response(request.Prompt)
     }()
 
-    // check whether user is legit
-    //      increment if true
+    if verify_user(request.ApiKey) {
+        answer := <-answerChannel
+        go savePromptAnswer(request.Prompt, request.ApiKey, answer)
 
-    // prepare logging struct
-
-    answer := <-answerChannel
-    go savePromptAnswer(request.Prompt, request.ApiKey, answer)
-
-    return &pb.LLMInference{Answer: answer}, nil
+        return &pb.LLMInference{Answer: answer}, nil
+    } else {
+        answer := "401 Error, Check your API key"
+        return &pb.LLMInference{Answer: answer}, nil
+    }
 }
 
 func main() {
+    // configure zap logger
+    logger = initLogger()
+    defer logger.Sync()
+
     // Set up a listener on port 50051
     lis, err := net.Listen("tcp", ":50051")
     if err != nil {
